9698ade5488cd15b0e76b77d38f48f46583b86c1
Voievodin1 <Oleksandr_Voievodin1@epam.com>
2017-11-23 17:07:29 +0200

    Some basic free monads

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Cats"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+// https://mvnrepository.com/artifact/org.typelevel/cats-free
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.0-RC1"
+libraryDependencies += "org.typelevel" % "cats-free_2.12" % "1.0.0-MF"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "0.5"
---
+++ src/main/scala/Application.scala
@@ -0,0 +1,67 @@
+import KVStore.KVStore
+import cats.data.State
+import cats.free.Free
+import cats.~>
+import KVStoreA.{Delete, Get, Put}
+
+sealed trait KVStoreA[A]
+
+object KVStoreA {
+  final case class Put[T](key: String, value: T) extends KVStoreA[Unit]
+  final case class Get[T](key: String) extends KVStoreA[Option[T]]
+  final case class Delete(key: String) extends KVStoreA[Unit]
+}
+
+object KVStore {
+
+  //Create Free type based on ADT
+
+  type KVStore[A] = Free[KVStoreA, A]
+
+  // Create smart constructors with liftF
+
+  import cats.free.Free.liftF
+
+  def put[T](key: String, value: T): KVStore[Unit] = liftF[KVStoreA, Unit](Put[T](key, value))
+
+  def get[T](key: String): KVStore[Option[T]] = liftF[KVStoreA, Option[T]](Get[T](key))
+
+  def delete(key: String): KVStore[Unit] = liftF(Delete(key))
+
+  def update[T](key: String, f: T => T): KVStore[Unit] =
+    for {
+      maybe <- get[T](key)
+      _ <- maybe.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
+    } yield ()
+}
+
+
+object Application extends App {
+
+  // Build a program
+
+  def program: KVStore[Option[Int]] =
+    for {
+      _ <- KVStore.put("black-cats", 2)
+      _ <- KVStore.update[Int]("black-cats", _ + 12)
+      _ <- KVStore.put("white-cats", 5)
+      n <- KVStore.get[Int]("black-cats")
+      _ <- KVStore.delete("white-cats")
+    } yield n
+
+  // Interpreter for the program
+
+  type KVStoreState[A] = State[Map[String, Any], A]
+
+  val pureInterpreter: KVStoreA ~> KVStoreState = new (KVStoreA ~> KVStoreState) {
+    def apply[A](storeManipulationFunction: KVStoreA[A]): KVStoreState[A] =
+      storeManipulationFunction match {
+        case Put(key, value) => State(map => map.updated())
+        case Get(key) => State(map => (map, map(key).asInstanceOf[A]))
+        case Delete(key) => State(map => (map - key, 0.asInstanceOf[A]))
+      }
+  }
+
+  val result: (Map[String, Any], Option[Int]) = program.foldMap(pureInterpreter).run(Map.empty).value
+  println(result)
+}
