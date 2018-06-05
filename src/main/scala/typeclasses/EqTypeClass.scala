0d55673e54dabed62f43d2385381e0eebaa887b7
Voievodin1 <Oleksandr_Voievodin1@epam.com>
2018-06-05 14:10:15 +0300

    some eq refactorings

--- src/main/scala/typeclasses/EqTypeClass.scala
+++ src/main/scala/typeclasses/EqTypeClass.scala
@@ -13,17 +13,25 @@ object EqTypeClass {
     implicit val eqString : Eq[String] = new Eq[String] {
       override def eqv(a: String, b: String): Boolean = a.equals(b)
     }
+
+    implicit def eqOption[A](implicit eqInst: Eq[A]): Eq[Option[A]] = new Eq[Option[A]] {
+      override def eqv(a: Option[A], b: Option[A]): Boolean =
+        (a, b) match {
+          case (Some(aVal), Some(bVal)) => eqInst.eqv(aVal, bVal)
+          case _ => false
+        }
+    }
   }
 
   object EqInterface {
-    def ===[A](a: A, b: A)(implicit eq: Eq[A]): Boolean = eq.eqv(a, b)
-    def =!=[A](a: A, b: A)(implicit eq: Eq[A]): Boolean = ! ===(a, b)
+    def ===[A](a: A, b: A)(implicit eqInst: Eq[A]): Boolean = eqInst.eqv(a, b)
+    def !==[A](a: A, b: A)(implicit eqInst: Eq[A]): Boolean = ! ===(a, b)
   }
 
   object EqSyntax {
     implicit class EqOps[A](lhs: A) {
       def ===(rhs: A)(implicit eq: Eq[A]): Boolean = eq.eqv(lhs, rhs)
-      def =!=(rhs: A)(implicit eq: Eq[A]): Boolean = ! ===(rhs)
+      def !==(rhs: A)(implicit eq: Eq[A]): Boolean = ! ===(rhs)
     }
   }
 }
@@ -36,5 +44,7 @@ object EqTypeClassTest extends App {
   Console.println(s"545 === 545 :: ${545 === 545}")
   Console.println(s"545 === 45415 :: ${545 === 45415}")
   Console.println(s"str === str :: ${"str" === "str"}")
-  Console.println(s"str === other_str :: ${"str " === "other_str"}")
+  Console.println(s"str === other_str :: ${"str" === "other_str"}")
+  Console.println(s"Option(str) === Option(other_str) :: ${Option("str") === Option("other_str")}")
+  Console.println(s"Option(545) === Option(545) :: ${Option(545) === Option(545)}")
 }
